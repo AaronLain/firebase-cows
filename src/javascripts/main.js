5abfc83de6824e5a8b6e045aba5d2fef8c810566
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-21 11:06:18 -0500

    buttons hide

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
+import login from './helpers/data/authData';
 import auth from './components/auth/auth';
+
 
 import 'bootstrap';
 import '../styles/main.scss';
@@ -8,6 +10,7 @@ import '../styles/main.scss';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  login.checkStatus();
   auth.loginButton();
 };
 
