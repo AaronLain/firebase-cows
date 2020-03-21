1723f29454b12ef9edef45f9307bf3a51135dd57
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-21 10:53:20 -0500

    auth stuff

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,14 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+import auth from './components/auth/auth';
+
 import 'bootstrap';
 import '../styles/main.scss';
 
-console.error('hi');
+
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  auth.loginButton();
+};
+
+init();
