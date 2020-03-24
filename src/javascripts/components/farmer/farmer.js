98436e6179c20ee297a7093e67e8fcac6dd00baa
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-23 21:00:41 -0500

    hover done

--- src/javascripts/components/farmer/farmer.js
+++ src/javascripts/components/farmer/farmer.js
@@ -1,9 +1,9 @@
 const farmerMaker = (farmer) => {
   let domString = '';
-  domString += '<div class="card">';
+  domString += '<div class="card farmer-card">';
   domString += '<div class="card-body">';
   domString += `<h5 class="card-title">${farmer.name}</h5>`;
-  domString += `<p class="card-text">weight: ${farmer.age}.</p>`;
+  domString += `<p class="card-text">Age: ${farmer.age}.</p>`;
   domString += '</div>';
   domString += '</div>';
 
